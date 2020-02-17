31413b17e39596509f797cde76ce2c240e686992
Bruno Gomes <brunosoges@gmail.com>
2020-02-16 22:18:13 -0300

    Resolução do exercicio, sem a interface, em progresso - part2

--- src/br/com/udemy/java/secao18/nointerface/model/services/RentalService.java
+++ src/br/com/udemy/java/secao18/nointerface/model/services/RentalService.java
@@ -1,6 +1,7 @@
 package br.com.udemy.java.secao18.nointerface.model.services;
 
 import br.com.udemy.java.secao18.nointerface.model.entities.CarRental;
+import br.com.udemy.java.secao18.nointerface.model.entities.Invoice;
 
 public class RentalService {
 	private Double pricePerDay;
@@ -13,9 +14,22 @@ public class RentalService {
 		this.pricePerHour = pricePerHour;
 		this.taxService = taxService;
 	}
-	
+
 	public void processInvoice(CarRental carRental) {
-		
+		long instant1 = carRental.getStart().getTime();
+		long instant2 = carRental.getFinish().getTime();
+		double hours = (double) (instant2 - instant1) / 1000 / 60 / 60;
+		double basicPayment;
+
+		if (hours <= 12.0) {
+			basicPayment = Math.ceil(hours) * pricePerHour;
+		} else {
+			basicPayment = Math.ceil(hours / 24) * pricePerDay;
+		}
+
+		double tax = taxService.tax(basicPayment);
+
+		carRental.setInvoice(new Invoice(basicPayment, tax));
 	}
 
 }
