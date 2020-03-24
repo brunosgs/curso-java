71b65ff40ae908c31b80d5fb752e366d562334cf
Bruno Gomes <brunosoges@gmail.com>
2020-03-24 13:39:10 -0300

    Inicializado aula de 191 de heração multipla e o problema de diamante

---
+++ src/br/com/udemy/java/secao18/interfaces/herancamult/application/MainHerancaMult.java
@@ -0,0 +1,9 @@
+package br.com.udemy.java.secao18.interfaces.herancamult.application;
+
+public class MainHerancaMult {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/br/com/udemy/java/secao18/interfaces/herancamult/devices/Scanner.java
@@ -0,0 +1,14 @@
+package br.com.udemy.java.secao18.interfaces.herancamult.devices;
+
+public class Scanner extends Device {
+
+	public Scanner(String serialNumber) {
+		super(serialNumber);
+	}
+
+	@Override
+	public void processDoc(String doc) {
+		
+	}
+	
+}
