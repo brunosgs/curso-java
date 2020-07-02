9e6d6bca3c751930dd6010b5ad369fa9ea965c40
Bruno Gomes <brunosoges@gmail.com>
2020-07-02 13:13:46 -0300

    Exercício resolvido, utilizando set e leitura de arquivo.

---
+++ src/br/com/udemy/java/secao19/exerciseresolved/application/MainExerciseResolved.java
@@ -0,0 +1,44 @@
+package br.com.udemy.java.secao19.exerciseresolved.application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+import br.com.udemy.java.secao19.exerciseresolved.entities.LogEntry;
+
+public class MainExerciseResolved {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter file full path: ");
+		String path = scan.nextLine();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			Set<LogEntry> set = new HashSet<>();
+			String line = br.readLine();
+
+			while (line != null) {
+				String[] fields = line.split(" ");
+				String userName = fields[0];
+				Date moment = Date.from(Instant.parse(fields[1]));
+
+				set.add(new LogEntry(userName, moment));
+
+				line = br.readLine();
+			}
+
+			System.out.println("Total Users: " + set.size());
+		} catch (IOException ioe) {
+			System.out.println("Error: " + ioe.getMessage());
+		}
+
+		scan.close();
+	}
+
+}
