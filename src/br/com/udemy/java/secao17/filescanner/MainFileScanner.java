44638e3139478ce4f06ab2ea0aa35e1cec448a53
Bruno Gomes <brunosoges@gmail.com>
2020-07-11 09:56:54 -0300

    Lendo arquivo de texto com classes File e Scanner.

---
+++ src/br/com/udemy/java/secao17/filescanner/MainFileScanner.java
@@ -0,0 +1,29 @@
+package br.com.udemy.java.secao17.filescanner;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MainFileScanner {
+
+	public static void main(String[] args) {
+		File file = new File("\\workspace\\ws-udemy\\curso-programacao\\src\\br\\com\\udemy\\java\\secao17\\filescanner\\in.txt");
+		Scanner scan = null;
+
+		try {
+			scan = new Scanner(file);
+
+			while (scan.hasNextLine()) {
+				System.out.println(scan.nextLine());
+			}
+
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		} finally {
+			if (scan != null) {
+				scan.close();
+			}
+		}
+	}
+
+}
