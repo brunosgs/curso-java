4803d6c2182376f67231f4588680f248fb7f282d
Bruno Gomes <brunosoges@gmail.com>
2020-07-11 12:48:44 -0300

    Ajuste nos pacotes do exercício de fixação.

---
+++ src/br/com/udemy/java/secao14/exerciciofixacao/entities/Individual.java
@@ -0,0 +1,35 @@
+package br.com.udemy.java.secao14.exerciciofixacao.entities;
+
+public class Individual extends TaxPayer {
+	private final static double TAX_15 = 0.15;
+	private final static double TAX_25 = 0.25;
+	private final static double TAX_50 = 0.5;
+	private Double healthExpenditures;
+
+	public Individual() {
+		super();
+	}
+
+	public Individual(String name, Double anualIncome, Double healthExpenditures) {
+		super(name, anualIncome);
+		this.healthExpenditures = healthExpenditures;
+	}
+
+	public Double getHealthExpenditures() {
+		return healthExpenditures;
+	}
+
+	public void setHealthExpenditures(Double healthExpenditures) {
+		this.healthExpenditures = healthExpenditures;
+	}
+
+	@Override
+	public double tax() {
+		if (this.getAnualIncome() < 20000) {
+			return (this.getAnualIncome() * TAX_15) - (this.healthExpenditures * TAX_50);
+		} else {
+			return (this.getAnualIncome() * TAX_25) - (this.healthExpenditures * TAX_50);
+		}
+	}
+
+}
---
+++ src/br/com/udemy/java/secao14/exerciciofixacao/entities/TaxPayer.java
@@ -0,0 +1,32 @@
+package br.com.udemy.java.secao14.exerciciofixacao.entities;
+
+public abstract class TaxPayer {
+	private String name;
+	private Double anualIncome;
+
+	public TaxPayer() {
+	}
+
+	public TaxPayer(String name, Double anualIncome) {
+		this.name = name;
+		this.anualIncome = anualIncome;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getAnualIncome() {
+		return anualIncome;
+	}
+
+	public void setAnualIncome(Double anualIncome) {
+		this.anualIncome = anualIncome;
+	}
+
+	public abstract double tax();
+}
